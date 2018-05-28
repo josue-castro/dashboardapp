ce1a122ace7bb52ea652e11df3a4efe5c8d817a3
josuecastro <josue.castro@upr.edu>
2018-05-27 23:36:36 -0400

    dashboard working

--- appjs/dashboard.js
+++ appjs/dashboard.js
@@ -6,18 +6,26 @@
 google.charts.load('current', {'packages': ['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(hashtagChart);
+
+google.charts.setOnLoadCallback(messageChart);
+
+google.charts.setOnLoadCallback(replyChart);
+
+google.charts.setOnLoadCallback(likeChart);
+
+google.charts.setOnLoadCallback(dislikeChart);
 
 function reformatData(jsonData){
-    var temp= jsonData.PartCounts;
+    var temp= jsonData.Result;
     console.log("temp: " + JSON.stringify(temp));
     var result = [];
     var i;
     var row;
     for (i=0; i < temp.length; ++i){
-        row= temp[i]
+        row= temp[i];
         dataElement = [];
-        dataElement.push(row.id + '-' + row.name);
+        dataElement.push(row.info);
         dataElement.push(row.count);
         result.push(dataElement);
     }
@@ -25,9 +33,9 @@ function reformatData(jsonData){
     return result;
 }
 
-function drawChart() {
+function hashtagChart() {
     var jsonData = $.ajax({
-        url: "http://localhost:5000/PartApp/parts/countbypartid",
+        url: "http://localhost:5000/MessageApp/dashboard/toptenhashtags",
         dataType: "json",
         async: false
     }).responseText;
@@ -36,30 +44,158 @@ function drawChart() {
 
     // Create our data table out of JSON data loaded from server.
     var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Parts');
-    data.addColumn('number', 'Stock');
+    data.addColumn('string', 'Hashtag');
+    data.addColumn('number', 'Tags');
     data.addRows(reformatData(JSON.parse(jsonData)));
 
     var options = {
-        title: 'Stock Parts by Id/name',
+        title: 'Trending Topics',
+        width: 400,
+        height: 300,
         chartArea: {width: '50%'},
         hAxis: {
-            title: 'Total Number',
+            title: 'Hashtag',
             minValue: 0
         },
         vAxis: {
-            title: 'Part'
+            title: 'Messages Tagged'
         }
     };
 
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
+    var chart = new google.visualization.ColumnChart(document.getElementById('hashtag_chart_div'));
     chart.draw(data, options);
-
 }
 
+function messageChart() {
+    var jsonData = $.ajax({
+        url: "http://localhost:5000/MessageApp/dashboard/dailymessages",
+        dataType: "json",
+        async: false
+    }).responseText;
 
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawChart);
+    console.log("jsonData: " + JSON.parse(jsonData));
 
+    // Create our data table out of JSON data loaded from server.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Messages');
+    data.addRows(reformatData(JSON.parse(jsonData)));
+
+    var options = {
+        title: 'Messages Per Day',
+        width: 400,
+        height: 300,
+        chartArea: {width: '50%'},
+        color: 'red',
+        hAxis: {
+            title: 'Date',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Total Messages Posted'
+        }
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('message_chart_div'));
+    chart.draw(data, options);
+}
+
+function replyChart() {
+    var jsonData = $.ajax({
+        url: "http://localhost:5000/MessageApp/dashboard/dailyreplies",
+        dataType: "json",
+        async: false
+    }).responseText;
+
+    console.log("jsonData: " + JSON.parse(jsonData));
+
+    // Create our data table out of JSON data loaded from server.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Replies');
+    data.addRows(reformatData(JSON.parse(jsonData)));
+
+    var options = {
+        title: 'Replies Per Day',
+        width: 400,
+        height: 300,
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Date',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Total Replies'
+        }
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('reply_chart_div'));
+    chart.draw(data, options);
+}
+
+function likeChart() {
+    var jsonData = $.ajax({
+        url: "http://localhost:5000/MessageApp/dashboard/dailylikes",
+        dataType: "json",
+        async: false
+    }).responseText;
+
+    console.log("jsonData: " + JSON.parse(jsonData));
+
+    // Create our data table out of JSON data loaded from server.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Likes');
+    data.addRows(reformatData(JSON.parse(jsonData)));
+
+    var options = {
+        title: 'Likes Per Day',
+        width: 400,
+        height: 300,
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Date',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Total Likes'
+        }
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('like_chart_div'));
+    chart.draw(data, options);
+}
+
+function dislikeChart() {
+    var jsonData = $.ajax({
+        url: "http://localhost:5000/MessageApp/dashboard/dailydislikes",
+        dataType: "json",
+        async: false
+    }).responseText;
+
+    console.log("jsonData: " + JSON.parse(jsonData));
+
+    // Create our data table out of JSON data loaded from server.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Dislikes');
+    data.addRows(reformatData(JSON.parse(jsonData)));
+
+    var options = {
+        title: 'Dislikes Per Day',
+        width: 400,
+        height: 300,
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Date',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Total Dislikes'
+        }
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('dislike_chart_div'));
+    chart.draw(data, options);
+}
 
